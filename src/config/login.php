c6f61c7188c69de1bdc2749dc2f4bc16bb9a8b8d
tienlb <tienlb@shinway.net>
2019-04-22 10:45:47 +0700

    0.3

--- src/config/login.php
+++ src/config/login.php
@@ -5,6 +5,6 @@ return [
         'username_field_name' => 'username',
         'password_field_name' => 'password',
         'sw_sv_url' => '',
-        'mode' => 1,
+        'mode' => 1, // 1:admin 2:user
     ]
 ];
